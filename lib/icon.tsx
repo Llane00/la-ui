97a2f2557d346583dda63350a8f078799f928fa9
Llane00 <bluetinestyle@126.com>
2019-09-05 16:07:26 +0800

    简化参数

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -7,13 +7,17 @@ interface IconProps extends React.SVGAttributes<SVGElement> {
   name: string;
 }
 
-const Icon: React.FunctionComponent<IconProps> = (props) => {
-  const {className, ...restProps} = props;
+const Icon: React.FunctionComponent<IconProps> = 
+  ({
+    className, 
+    name, 
+    ...restProps
+  }) => {
   return (
     <svg className={classes('lui-icon', className)} 
       {...restProps}
     >
-      <use xlinkHref={`#${props.name}`} />
+      <use xlinkHref={`#${name}`} />
     </svg>
   );
 };
