fafedd48ba73b2e6086facb6399123175b9f193a
Llane00 <bluetinestyle@126.com>
2019-09-12 11:42:31 +0800

    add examples

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,10 +2,13 @@ const base = require('./webpack.config');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = Object.assign({}, base, {
   mode: 'development',
+  entry: {
+    example: './example/index.tsx'
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'llane-ui',
-      template: 'index.html'
+      template: 'example.html'
     })
   ],
 })
