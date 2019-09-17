251946f3ae7fd2f05c0575769753826dd3fee3f1
Llane00 <bluetinestyle@126.com>
2019-09-17 10:27:58 +0800

    fix jest bug 3

--- lib/icon/__tests__/icon.unit.jsx
+++ lib/icon/__tests__/icon.unit.jsx
@@ -1,17 +1,17 @@
-import {mount} from 'enzyme'
 import renderer from 'react-test-renderer'
 import React from 'react'
 import Icon from '../icon'
+import { mount } from 'enzyme'
 
 describe('icon', () => {
-    it('render successfully', ()=>{
-      const json = renderer.create(<Icon name="alipay"/>).toJSON()
-      expect(json).toMatchSnapshot()
-    })
-    it('click', ()=>{
-      const fn = jest.fn()
-      const component = mount(<Icon name="alipay" onClick={fn}/>)
-      component.find('svg').simulate('click')
-      expect(fn).toBeCalled()
-    })
+  it('render successfully', () => {
+    const json = renderer.create(<Icon name="alipay" />).toJSON()
+    expect(json).toMatchSnapshot()
+  })
+  it('onClick', () => {
+    const fn = jest.fn()
+    const component = mount(<Icon name="alipay" onClick={fn} />)
+    component.find('svg').simulate('click')
+    expect(fn).toBeCalled()
+  })
 })
