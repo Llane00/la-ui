4f3673d83357a14b456a57e0f4e195b668380445
Llane00 <bluetinestyle@126.com>
2019-09-23 14:24:06 +0800

    add: Layout组件导出规则修改

--- lib/layout/layout.tsx
+++ lib/layout/layout.tsx
@@ -27,3 +27,8 @@ const Layout: React.FunctionComponent<Porps> = ({
 };
 
 export default Layout;
+export {Layout};
+export {default as Header} from './header';
+export {default as Sider} from './sider';
+export {default as Content} from './content';
+export {default as Footer} from './footer';
