4ca47befd1e34a33eb14b4ae0cd3a896cd57677b
Llane00 <bluetinestyle@126.com>
2019-09-25 16:22:39 +0800

    add: 添加Button组件单元测试

--- lib/button/__tests__/button.unit.tsx
+++ lib/button/__tests__/button.unit.tsx
@@ -1,10 +1,22 @@
 import renderer from 'react-test-renderer'
 import React from 'react'
 import Button from '../button'
+import { scopedClassMaker } from '../../helpers/classes'
 
 describe('button', () => {
-    it('button is div element', ()=>{
+    it('button组件测试', ()=>{
       const json = renderer.create(<Button btnType='default'>测试</Button>).toJSON()
       expect(json).toMatchSnapshot()
     })
+})
+
+describe('scopedClassMaker', () => {
+  it('', ()=>{
+    const sc = scopedClassMaker('la-ui-button');
+    expect(sc('')).toEqual('la-ui-button');
+    expect(sc('x')).toEqual('la-ui-button-x');
+    expect(sc({'': true})).toEqual('la-ui-button');
+    expect(sc({'': true, 'default': false})).toEqual('la-ui-button');
+    expect(sc({'': true, 'default': false}, {extra: 'red'})).toEqual('la-ui-button red');
+  })
 })
